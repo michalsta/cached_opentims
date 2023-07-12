e52c9a782ef13db42032d485a5dc3d55d371db3b
Michał Startek <michal.startek@mimuw.edu.pl>
2023-07-12 23:13:30 +0000

    Improvements

--- cached_opentims/main.py
+++ cached_opentims/main.py
@@ -33,7 +33,7 @@ def count(frames, min_scan, max_scan, counts):
     return ret
 
 @numba.njit()
-def fill_data(frames, scan_min, scan_max, starts, counts, src, T):
+def fill_data(frames, scan_min, scan_max, mz_min, starts, counts, src, T):
     t_idx = 0
     for frame in frames:
         for scan in range(scan_min, scan_max+1):
@@ -42,6 +42,33 @@ def fill_data(frames, scan_min, scan_max, starts, counts, src, T):
                 T[t_idx] = src[d_start+d_idx]
                 t_idx += 1
                 d_idx += 1
+
+
+@numba.njit()
+def fill_data(frames, scan_min, scan_max, starts, counts, src, T):
+    t_idx = 0
+    for frame in frames:
+        for scan in range(scan_min, scan_max+1):
+            d_start = starts[frame, scan]
+            for d_idx in range(0, counts[frame, scan]):
+                T[t_idx] = src[d_start+d_idx]
+                t_idx += 1
+
+
+@numba.njit()
+def mz_filtered_indices(frames, scan_min, scan_max, starts, counts, src, T):
+    ret = np.empty(shape=count(frames, min_scan, max_scan, counts), dtype=uint64)
+    t_idx = 0
+    for frame in frames:
+        for scan in range(scan_min, scan_max+1):
+            d_start = starts[frame, scan]
+            for d_idx in range(0, counts[frame, scan]):
+                if mz_min <= masses[d_start+d_idx] <= mz+max:
+                    T[t_idx] = src[d_start+d_idx]
+                    t_idx += 1
+    return ret[:t_idx]
+
+
 
 class CachedOpenTIMS:
     def __init__(self, path, tmpdir = None):
@@ -55,25 +82,37 @@ class CachedOpenTIMS:
                     #print(frame)
                     DW.append_df(pd.DataFrame(frame))
 
-        self.backend = mmapped_df.open_dataset(cache_dir)
+            self.backend = mmapped_df.open_dataset(cache_dir)
 
-        self.starts = np.zeros(shape=(self.OT.max_frame+1, self.OT.max_scan+1), dtype=np.uint64)
-        self.counts = np.zeros(shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64)
-        self.index = index(self.backend.frame.values, self.backend.scan.values, self.starts, self.counts)
+            self.starts = np.zeros(shape=(self.OT.max_frame+1, self.OT.max_scan+1), dtype=np.uint64)
+            self.counts = np.zeros(shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64)
+            self.index = index(self.backend.frame.values, self.backend.scan.values, self.starts, self.counts)
+            np.save(cache_dir / 'starts.npy')
+            np.save(cache_dir / 'counts.npy')
+        else:
+            self.backend = mmapped_df.open_dataset(cache_dir)
+            self.starts = np.load(cache_dir / 'starts.npy')
+            self.counts = np.load(cache_dir / 'counts.npy')
 
-    def query(self, frames):
-        return box_query(frames, self.OT.min_scan, self.OT.max_scan)
+    def query(self, frames, colnames=None):
+        return box_query(frames, self.OT.min_scan, self.OT.max_scan, colnames)
 
-    def box_query(self, frames, min_scan, max_scan):
+    def box_query(self, frames, min_scan, max_scan, colnames):
         if not isinstance(frames, np.ndarray):
             frames = np.array(frames, dtype=np.uint64)
         arr_size = count(frames, min_scan, max_scan, self.counts)
         acc = {}
-        for colname in self.backend:
+        for colname in (self.backend if conlnames is None else colnames):
             T = np.empty(shape=(arr_size,), dtype=self.backend.dtypes[colname])
             fill_data(frames, min_scan, max_scan,self.starts,self.counts,self.backend[colname].values,T)
             acc[colname] = T
         return pd.DataFrame(acc)
+
+#    def mz_box_query(self, frames, min_scan, max_scan, mz_min, mz_max):
+#        if not isinstance(frames, np.ndarray):
+#            frames = np.array(frames, dtype=np.uint64)
+#        indices =
+
 
 if __name__ == '__main__':
     OT = CachedOpenTIMS("/mnt/storage/science/midia_rawdata/8027.d")

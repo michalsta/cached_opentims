22d607f12bbebc5f6b9bdbd0a488d4f335fa0fb0
Michał Startek <michal.startek@mimuw.edu.pl>
2023-09-12 14:05:47 +0200

    [REFORMAT]

--- cached_opentims/main.py
+++ cached_opentims/main.py
@@ -7,7 +7,6 @@ from opentimspy import OpenTIMS
 import mmapped_df
 
 
-
 @numba.njit
 def index(frames, scans, starts, counts):
     for idx in range(len(frames)):
@@ -15,7 +14,7 @@ def index(frames, scans, starts, counts):
         scan = scans[idx]
         if starts[frame, scan] == 0:
             starts[frame, scan] = idx
-        counts[frame,scan] += 1
+        counts[frame, scan] += 1
 
     last_frame = 0
     for frame in range(starts.shape[0]):
@@ -24,22 +23,24 @@ def index(frames, scans, starts, counts):
                 starts[frame, scan] = last_frame
             last_frame = starts[frame, scan]
 
+
 @numba.njit()
 def count(frames, min_scan, max_scan, counts):
     ret = np.uint64(0)
     for frame in frames:
-        for scan in range(min_scan, max_scan+1):
+        for scan in range(min_scan, max_scan + 1):
             ret += counts[frame, scan]
     return ret
+
 
 @numba.njit()
 def fill_data(frames, scan_min, scan_max, mz_min, starts, counts, src, T):
     t_idx = 0
     for frame in frames:
-        for scan in range(scan_min, scan_max+1):
+        for scan in range(scan_min, scan_max + 1):
             d_start = starts[frame, scan]
             for d_idx in range(0, counts[frame, scan]):
-                T[t_idx] = src[d_start+d_idx]
+                T[t_idx] = src[d_start + d_idx]
                 t_idx += 1
                 d_idx += 1
 
@@ -48,10 +49,10 @@ def fill_data(frames, scan_min, scan_max, mz_min, starts, counts, src, T):
 def fill_data(frames, scan_min, scan_max, starts, counts, src, T):
     t_idx = 0
     for frame in frames:
-        for scan in range(scan_min, scan_max+1):
+        for scan in range(scan_min, scan_max + 1):
             d_start = starts[frame, scan]
             for d_idx in range(0, counts[frame, scan]):
-                T[t_idx] = src[d_start+d_idx]
+                T[t_idx] = src[d_start + d_idx]
                 t_idx += 1
 
 
@@ -60,39 +61,47 @@ def mz_filtered_indices(frames, scan_min, scan_max, starts, counts, src, T):
     ret = np.empty(shape=count(frames, min_scan, max_scan, counts), dtype=uint64)
     t_idx = 0
     for frame in frames:
-        for scan in range(scan_min, scan_max+1):
+        for scan in range(scan_min, scan_max + 1):
             d_start = starts[frame, scan]
             for d_idx in range(0, counts[frame, scan]):
-                if mz_min <= masses[d_start+d_idx] <= mz+max:
-                    T[t_idx] = src[d_start+d_idx]
+                if mz_min <= masses[d_start + d_idx] <= mz + max:
+                    T[t_idx] = src[d_start + d_idx]
                     t_idx += 1
     return ret[:t_idx]
 
 
-
 class CachedOpenTIMS:
-    def __init__(self, path, tmpdir = None):
+    def __init__(self, path, tmpdir=None):
         path = Path(path)
         assert tmpdir is None
-        cache_dir = Path(str(path)+".cache")
+        cache_dir = Path(str(path) + ".cache")
         self.OT = OpenTIMS(path)
         if not cache_dir.exists():
             with mmapped_df.DatasetWriter(cache_dir) as DW:
                 for frame in self.OT:
-                    #print(frame)
+                    # print(frame)
                     DW.append_df(pd.DataFrame(frame))
 
             self.backend = mmapped_df.open_dataset(cache_dir)
 
-            self.starts = np.zeros(shape=(self.OT.max_frame+1, self.OT.max_scan+1), dtype=np.uint64)
-            self.counts = np.zeros(shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64)
-            self.index = index(self.backend.frame.values, self.backend.scan.values, self.starts, self.counts)
-            np.save(cache_dir / 'starts.npy', self.starts)
-            np.save(cache_dir / 'counts.npy', self.counts)
+            self.starts = np.zeros(
+                shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64
+            )
+            self.counts = np.zeros(
+                shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64
+            )
+            self.index = index(
+                self.backend.frame.values,
+                self.backend.scan.values,
+                self.starts,
+                self.counts,
+            )
+            np.save(cache_dir / "starts.npy", self.starts)
+            np.save(cache_dir / "counts.npy", self.counts)
         else:
             self.backend = mmapped_df.open_dataset(cache_dir)
-            self.starts = np.load(cache_dir / 'starts.npy')
-            self.counts = np.load(cache_dir / 'counts.npy')
+            self.starts = np.load(cache_dir / "starts.npy")
+            self.counts = np.load(cache_dir / "counts.npy")
 
     def query(self, frames, colnames=None):
         return box_query(frames, self.OT.min_scan, self.OT.max_scan, colnames)
@@ -102,11 +111,20 @@ class CachedOpenTIMS:
             frames = np.array(frames, dtype=np.uint64)
         arr_size = count(frames, min_scan, max_scan, self.counts)
         acc = {}
-        for colname in (self.backend if colnames is None else colnames):
+        for colname in self.backend if colnames is None else colnames:
             T = np.empty(shape=(arr_size,), dtype=self.backend.dtypes[colname])
-            fill_data(frames, min_scan, max_scan,self.starts,self.counts,self.backend[colname].values,T)
+            fill_data(
+                frames,
+                min_scan,
+                max_scan,
+                self.starts,
+                self.counts,
+                self.backend[colname].values,
+                T,
+            )
             acc[colname] = T
         return pd.DataFrame(acc)
+
 
 #    def mz_box_query(self, frames, min_scan, max_scan, mz_min, mz_max):
 #        if not isinstance(frames, np.ndarray):
@@ -114,6 +132,6 @@ class CachedOpenTIMS:
 #        indices =
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     OT = CachedOpenTIMS("/mnt/storage/science/midia_rawdata/8027.d")
-    print(OT.box_query([345,645,746], 100, 500))
+    print(OT.box_query([345, 645, 746], 100, 500))
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from glob import glob
 
 
 setup(
-    name='cached_opentims',
-    version='0.0.1',
-    url='https://github.com/michalsta/cached_opentims',
-    author='Michał Startek',
-    author_email='author@gmail.com',
-    description='Memory-mapped, on-disk-extraced, indexed access to TIMS TOF data',
-    packages=find_packages(),    
-    install_requires=['numpy', 'pandas', 'mmapped_df'],
-    scripts=glob("tools/*.py")
+    name="cached_opentims",
+    version="0.0.1",
+    url="https://github.com/michalsta/cached_opentims",
+    author="Michał Startek",
+    author_email="author@gmail.com",
+    description="Memory-mapped, on-disk-extraced, indexed access to TIMS TOF data",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "mmapped_df"],
+    scripts=glob("tools/*.py"),
 )

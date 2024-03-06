939bb95afb6a085b8440db98e6540aedf02493b4
matteo <matteo.lacki@gmail.com>
2024-03-06 10:00:23 +0100

    CachedOpenTIMS and matteos code can use the same .startrek dump.

--- cached_opentims/io.py
+++ cached_opentims/io.py
@@ -1,8 +1,10 @@
+import typing
 from pathlib import Path
 
 from tqdm import tqdm as progressbar
 
 import mmapped_df
+import numba
 import numpy as np
 import numpy.typing as npt
 import opentimspy
@@ -33,49 +35,97 @@ def read_memmapped_events_file(path, mode="r"):
     return np.memmap("memmapped", dtype=np.uint32, mode=mode).reshape(-1, 4)
 
 
-def dump_tdf_to_startrek(
-    rawdata_path: Path,
-    output_folder: Path,
-    _progressbar_message: str = "Dumping raw data to `.startrek`.",
-    _columns=[
+@numba.njit
+def index(frames, scans, starts, counts):
+    for idx in range(len(frames)):
+        frame = frames[idx]
+        scan = scans[idx]
+        if starts[frame, scan] == 0:
+            starts[frame, scan] = idx
+        counts[frame, scan] += 1
+
+    last_frame = 0
+    for frame in range(starts.shape[0]):
+        for scan in range(starts.shape[1]):
+            if starts[frame, scan] == 0:
+                starts[frame, scan] = last_frame
+            last_frame = starts[frame, scan]
+
+
+def create_and_open_cached_tdf(
+    folder_startrek: Path | str,
+    folder_d: Path | str | None = None,
+    _progressbar_message: str = "Dumping raw data to mmapped format.",
+    _columns: list[str] = [
         "frame",
         "scan",
         "tof",
         "intensity",
-        "retention_time",
-        "inv_ion_mobility",
         "mz",
+        "inv_ion_mobility",
+        "retention_time",
     ],
-    **kwargs,
-) -> None:
-    raw_data_handler = opentimspy.OpenTIMS(rawdata_path)
-    startrek_handler = mmapped_df.DatasetWriter(output_folder, **kwargs)
-    frame_scan_counts = np.zeros(
-        dtype=np.uint32,
-        shape=(raw_data_handler.max_frame + 1, raw_data_handler.max_scan + 1),
-    )
-    maxes = []
-    for frame in progressbar(
-        range(raw_data_handler.min_frame, raw_data_handler.max_frame + 1),
-        desc=_progressbar_message,
-    ):
-        raw = raw_data_handler.query(frame, columns=_columns)
-        raw = pd.DataFrame(raw, columns=_columns, copy=False)
-        maxes.append({c: raw[c].max() for c in raw.columns})
-        count2D(frame_scan_counts, raw.frame.to_numpy(), raw.scan.to_numpy())
-        startrek_handler.append_df(raw)
-    maxes = pd.DataFrame(maxes)
-    maxes = pd.DataFrame([{c: maxes[c].max() for c in maxes.columns}])
-    np.save(output_folder / "counts.npy", frame_scan_counts)
-    maxes.to_parquet(output_folder / "maxes.npy")
-    pd.DataFrame(raw_data_handler.frames).to_parquet(output_folder / "frames.parquet")
+) -> tuple[pd.DataFrame, npt.NDArray, npt.NDArray, dict[str, typing.Any], pd.DataFrame]:
+    """
+    Dump raw tdf format into startrek format if necessary and then open it.
+    Dump also basic statistics such as maxes, and tables mapping frame and scan to start (whatever it is) and counts of events.
 
+    Arguments:
+        folder_d (Path|str|None): Path to the .d folder with data in the tdf format. Might be neglected when the 'folder_startrek' exists.
+        folder_startrek (Path|str): Path to the output folder in the startrek format.
 
-def read_startrek(
-    startrek_path: Path,
-) -> tuple[pd.DataFrame, npt.NDArray, npt.NDArray, pd.DataFrame]:
-    df = mmapped_df.open_dataset(startrek_path)
-    frame_scan_counts = np.load(startrek_path / "counts.npy")
-    maxes = pd.read_parquet(startrek_path / "maxes.npy")
-    frames = pd.read_parquet(startrek_path / "frames.parquet")
-    return df, frame_scan_counts, maxes, frames
+    Returns:
+        tuple: data frame with mmapped raw data, starts, counts, maxes, and the original Frames table from the 'analysis.tdf'.
+
+    Raises:
+        no concerns.
+    """
+    folder_startrek = Path(folder_startrek)
+
+    _fresh = not folder_startrek.exists()
+    if _fresh:
+        assert folder_d is not None
+        folder_d = Path(folder_d)
+        raw_data_handler = opentimspy.OpenTIMS(folder_d)
+        with mmapped_df.DatasetWriter(folder_startrek, overwrite_dir=True) as DW:
+            for frame in progressbar(
+                raw_data_handler.frames["Id"],
+                desc=_progressbar_message,
+            ):
+                raw_frame = pd.DataFrame(
+                    raw_data_handler.query(frame, columns=_columns),
+                    columns=_columns,
+                    copy=False,
+                )
+                DW.append_df(raw_frame)
+
+    df = mmapped_df.open_dataset(folder_startrek)
+
+    if _fresh:
+        maxes = {c: np.max(df[c].to_numpy()) for c in df.columns}
+        assert maxes["frame"] <= raw_data_handler.max_frame
+        assert maxes["scan"] <= raw_data_handler.max_scan
+        stats_shape = raw_data_handler.max_frame + 1, raw_data_handler.max_scan + 1
+        pd.DataFrame([maxes]).to_parquet(folder_startrek / "maxes.parquet")
+
+        starts = np.zeros(shape=stats_shape, dtype=np.uint64)
+        counts = np.zeros(shape=stats_shape, dtype=np.uint64)
+        index(
+            df.frame.to_numpy(),
+            df.scan.to_numpy(),
+            starts,
+            counts,
+        )
+        np.save(folder_startrek / "counts.npy", counts)
+        np.save(folder_startrek / "starts.npy", starts)
+        Frames = pd.DataFrame(raw_data_handler.frames)
+        Frames.to_parquet(folder_startrek / "frames.parquet")
+    else:
+        counts = np.load(folder_startrek / "counts.npy")
+        starts = np.load(folder_startrek / "starts.npy")
+        maxes = pd.read_parquet(folder_startrek / "maxes.parquet").to_dict(
+            orient="records"
+        )[0]
+        Frames = pd.read_parquet(folder_startrek / "frames.parquet")
+
+    return df, starts, counts, maxes, Frames
--- cached_opentims/main.py
+++ cached_opentims/main.py
@@ -7,24 +7,8 @@ import mmapped_df
 import numba
 import numpy as np
 import pandas as pd
+from cached_opentims.io import create_and_open_cached_tdf
 from opentimspy import OpenTIMS
-
-
-@numba.njit
-def index(frames, scans, starts, counts):
-    for idx in range(len(frames)):
-        frame = frames[idx]
-        scan = scans[idx]
-        if starts[frame, scan] == 0:
-            starts[frame, scan] = idx
-        counts[frame, scan] += 1
-
-    last_frame = 0
-    for frame in range(starts.shape[0]):
-        for scan in range(starts.shape[1]):
-            if starts[frame, scan] == 0:
-                starts[frame, scan] = last_frame
-            last_frame = starts[frame, scan]
 
 
 @numba.njit()
@@ -36,16 +20,16 @@ def count(frames, min_scan, max_scan, counts):
     return ret
 
 
-@numba.njit()
-def fill_data(frames, scan_min, scan_max, mz_min, starts, counts, src, T):
-    t_idx = 0
-    for frame in frames:
-        for scan in range(scan_min, scan_max + 1):
-            d_start = starts[frame, scan]
-            for d_idx in range(0, counts[frame, scan]):
-                T[t_idx] = src[d_start + d_idx]
-                t_idx += 1
-                d_idx += 1
+# @numba.njit()
+# def fill_data(frames, scan_min, scan_max, mz_min, starts, counts, src, T):
+#     t_idx = 0
+#     for frame in frames:
+#         for scan in range(scan_min, scan_max + 1):
+#             d_start = starts[frame, scan]
+#             for d_idx in range(0, counts[frame, scan]):
+#                 T[t_idx] = src[d_start + d_idx]
+#                 t_idx += 1
+#                 d_idx += 1
 
 
 @numba.njit()
@@ -78,43 +62,18 @@ class CachedOpenTIMS:
         path = Path(path)
         assert tmpdir is None
         cache_dir = Path(str(path) + ".cache")
-        self.OT = OpenTIMS(path)
 
-        try:
-            self.backend = mmapped_df.open_dataset(cache_dir)
-            self.starts = np.load(cache_dir / "starts.npy")
-            self.counts = np.load(cache_dir / "counts.npy")
-
-        except FileNotFoundError:
-            if dont_recalculate:
-                raise
-
-            with mmapped_df.DatasetWriter(cache_dir, overwrite_dir=True) as DW:
-                for frame in tqdm(
-                    self.OT,
-                    total=len(self.OT.frames["Id"]),
-                    desc=sys.argv[0]
-                    + ": uncompressing TIMS dataset into cached_opentims",
-                ):
-                    # print(frame)
-                    DW.append_df(pd.DataFrame(frame))
-
-            self.backend = mmapped_df.open_dataset(cache_dir)
-
-            self.starts = np.zeros(
-                shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64
-            )
-            self.counts = np.zeros(
-                shape=(self.OT.max_frame + 1, self.OT.max_scan + 1), dtype=np.uint64
-            )
-            self.index = index(
-                self.backend.frame.values,
-                self.backend.scan.values,
-                self.starts,
-                self.counts,
-            )
-            np.save(cache_dir / "starts.npy", self.starts)
-            np.save(cache_dir / "counts.npy", self.counts)
+        (
+            self.backend,
+            self.starts,
+            self.counts,
+            _maxes,
+            _Frames,
+        ) = create_and_open_cached_tdf(
+            folder_startrek=cache_dir,
+            folder_d=path,
+            _progressbar_message="Uncompressing TIMS dataset into *.d.cache",
+        )
 
     def query(self, frames, colnames=None):
         return box_query(frames, self.OT.min_scan, self.OT.max_scan, colnames)
